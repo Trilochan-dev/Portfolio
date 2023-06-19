66b918572642702aa851003ebdf4e7d6fdb3e9de
Trilochan-dev <127761645+Trilochan-dev@users.noreply.github.com>
2023-06-19 14:45:00 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className='h-screen bg-primary-dark'>
+  return (<>
+    <Head>
+      <title>Trilochan Behera</title>
+      <meta name="description" content="Trilochan Behera Portfolio website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/Images/welcome.gif" />
+    </Head>
+    <div className='bg-primary-dark'>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </div>
+  </>
   )
-
-
-
 }
