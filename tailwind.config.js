2fc09a63e2cca58e239f599f4a836d40af56f02b
Trilochan-dev <127761645+Trilochan-dev@users.noreply.github.com>
2023-06-19 14:47:26 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,16 +40,19 @@ module.exports = {
         Raleway: '"Raleway", sans-serif',
       },
       screens: {
-        xlg:"1349px",
+        xlg: "1349px",
         xs: "480px",
       },
       gridTemplateColumns: {
         // Simple 16 column grid
-        '13': 'repeat(13, minmax(0, 1fr))',
+        13: "repeat(13, minmax(0, 1fr))",
 
         // Complex site-specific column configuration
-        'footer': '200px minmax(900px, 1fr) 100px',
-      }
+        footer: "200px minmax(900px, 1fr) 100px",
+      },
+      maxWidth: {
+        final: "1640px",
+      },
     },
   },
   plugins: [],
