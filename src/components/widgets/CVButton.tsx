821a84513d5c49bc83035b0e86a9497080276956
Trilochan <127761645+trilochan-behera-dev@users.noreply.github.com>
2023-09-23 20:59:22 +0530

    Update CVButton.tsx

--- src/components/widgets/CVButton.tsx
+++ src/components/widgets/CVButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CVButton({ title, ...props }: any) {
 
     return (
-        <Link href="./Resume/Trilochan_resume.pdf" download="Trilochan_Behera_Resume" target="_blank">
+        <Link href="./Resume/Trilochan_Behera_Resume.pdf" download="Trilochan_Behera_Resume" target="_blank">
             <button className={`bg-emerald-500 text-white px-4 py-1 my-4 rounded-lg h-10 text-lg font-bold flex items-center gap-2 shadow-2xl`}>
                 <div className="flex items-center justify-center gap-2">
                     {title} <span className="pt-1">{props?.icon}</span> 
